24a3ea2635184abf4767a74a2b1e2bbea7308c3d
Felipe <felipedossantosflor@gmail.com>
2022-07-18 10:53:31 -0300

    Tabuada / multiplication table

--- Tabuada/Tabuada/Program.cs
+++ Tabuada/Tabuada/Program.cs
@@ -6,14 +6,21 @@ namespace Tabuada
     {
         static void Main(string[] args)
         {
-            int number;
-            Console.WriteLine("Hello, please type number: ");
-            number = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Multiplicate table of the number: " + number);
+            int number, answer;
 
-            for (int i = 0; i <= 10; i++) {
-                Console.WriteLine(number + "x" + i + "=" + number * i); 
-            }
+            do
+            {
+                Console.WriteLine("Hello, please type number: ");
+                number = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Multiplicate table of the number: " + number);
+
+                for (int i = 0; i <= 10; i++)
+                {
+                    Console.WriteLine(number + "x" + i + "=" + number * i);
+                }
+                Console.WriteLine("Do you want to recalculate? 1 to continue ou 2");
+                answer = Convert.ToInt32(Console.ReadLine());
+            } while (answer == 1);
             
         }
     }
